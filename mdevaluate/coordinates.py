bb9e5c1c3803d49f48b8389a17cdaf41d6b48327
Niels Müller <post@nielsm.de>
2016-02-08 13:27:29 +0100

    New slicing functionality for Coordinates class Coordinates objects may e sliced like otheriterables like: This will return an iterator which yields the respective frames.

--- mdevaluate/coordinates.py
+++ mdevaluate/coordinates.py
@@ -2,7 +2,13 @@ import numpy as np
 from .atoms import AtomSubset
 from .pbc import pbc_diff
 
+
 class Coordinates:
+    """
+    Coordinates represent trajectory data, which is used for evaluation functions.
+
+    Atoms may be selected by specifing a atom_subset or a atom_filter.
+    """
     atom_filter = None
 
     def __init__(self, frames, atom_filter=None, atom_subset: AtomSubset=None):
@@ -17,20 +23,29 @@ class Coordinates:
             self.atom_subset = atom_subset
             self.atoms = atom_subset.atoms
 
+    def slice(self, slice):
+        for i in range(len(self))[slice]:
+            yield self[i]
+
+            
     def __getitem__(self, item):
-        try:
-            if self.atom_filter is not None:
-                return self.frames.__getitem__(item).coordinates[self.atom_filter]
-            else:
-                return self.frames.__getitem__(item).coordinates
-        except EOFError:
-            raise IndexError
+        if isinstance(item, slice):
+            return self.slice(item)
+        else:
+            try:
+                if self.atom_filter is not None:
+                    return self.frames.__getitem__(item).coordinates[self.atom_filter]
+                else:
+                    return self.frames.__getitem__(item).coordinates
+            except EOFError:
+                raise IndexError
 
     def __len__(self):
         return len(self.frames)
 
 
 class MeanCoordinates(Coordinates):
+
     def __init__(self, frames, atom_filter=None, mean=1):
         super().__init__(frames, atom_filter)
         self.mean = mean
@@ -38,34 +53,38 @@ class MeanCoordinates(Coordinates):
 
     def __getitem__(self, item):
         frame = super().__getitem__(item)
-        for i in range(item+1, item+self.mean):
+        for i in range(item + 1, item + self.mean):
             frame += super().__getitem__(i)
 
-        return frame/self.mean
+        return frame / self.mean
+
     def len(self):
-        return len(super()-self.mean+1)
+        return len(super() - self.mean + 1)
 
 
 class CoordinatesMap:
+
     def __init__(self, coordinates, function):
         self.coordinates = coordinates
         self.function = function
-    
+
     def __getitem__(self, item):
         return self.function(self.coordinates.__getitem__(item))
-    
+
     def __len__(self):
         return len(self.coordinates.frames)
 
+
 def map_coordinates(fn):
     def wrapped(coordinates, *args, **kwargs):
-        return CoordinatesMap(coordinates, lambda x:fn(x, *args, **kwargs))
+        return CoordinatesMap(coordinates, lambda x: fn(x, *args, **kwargs))
     return wrapped
+
 
 @map_coordinates
 def centers_of_mass(c, masses):
     """
-    
+
     A- 1
     B- 2
     A- 1
@@ -73,23 +92,23 @@ def centers_of_mass(c, masses):
     A-
     B-
     A-
-    C 
+    C
     A-
     B-
     A-
     C
-    
-    
+
+
     Example:
     rd = XTCReader('t.xtc')
     coordinates = Coordinates(rd)
     com = centers_of_mass(coordinates, (1.0, 2.0, 1.0, 3.0))
-    
+
     """
     # At first, regroup our array
     number_of_masses = len(masses)
-    number_of_coordinates,number_of_dimensions = c.shape
+    number_of_coordinates, number_of_dimensions = c.shape
     number_of_new_coordinates = number_of_coordinates // number_of_masses
     grouped_masses = c.reshape(number_of_new_coordinates, number_of_masses, number_of_dimensions)
-    
+
     return np.average(grouped_masses, axis=1, weights=masses)
