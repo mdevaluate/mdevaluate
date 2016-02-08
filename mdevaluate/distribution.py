cff50ced0cfba05a707688f474e3daae81a3211d
Niels Müller <post@nielsm.de>
2016-02-08 13:24:42 +0100

    New function to compute the radial density

--- mdevaluate/distribution.py
+++ mdevaluate/distribution.py
@@ -3,8 +3,67 @@ from .coordinates import pbc_diff
 from .atoms import next_neighbors
 
 
-def time_average(function, coordinates, pool=None):
+def rotate_axis(coords, axis):
+    """
+    Rotate a set of coordinates to a given axis.
+    """
+    axis = np.array(axis) / norm(axis)
+    zaxis = np.array([0, 0, 1])
+    if (axis == zaxis).sum() == 3:
+        return coords
+    rotation_axis = np.cross(axis, zaxis)
+    rotation_axis = rotation_axis / np.linalg.norm(rotation_axis)
 
+    theta = np.arccos(axis @ zaxis / np.linalg.norm(axis))
+
+    # return theta/pi, rotation_axis
+
+    ux, uy, uz = rotation_axis
+    cross_matrix = array([
+        [0, -uz, uy],
+        [uz,  0, -ux],
+        [-uy, ux, 0]
+    ])
+    rotation_matrix = np.cos(theta) * np.identity(len(axis)) \
+        + (1 - np.cos(theta)) * rotation_axis.reshape(-1, 1) @ rotation_axis.reshape(1, -1) \
+        + np.sin(theta) * cross_matrix
+
+    if len(coords.shape) == 2:
+        rotated = np.array([rotation_matrix @ xyz for xyz in coords])
+    else:
+        rotated = rotation_matrix @ coords
+    return rotated
+
+
+def polar_coordinates(x, y):
+    """Convert cartesian to polar coordinates."""
+    radius = np.sqrt(x**2 + y**2)
+    phi = np.arctan(y / x)
+    return radius, phi
+
+
+def spherical_coordinates(x, y, z):
+    """Convert cartesian to spherical coordinates."""
+    radius, phi = polar_coordinates(x,y)
+    theta = np.arccos(z / radius)
+    return radius, phi, theta
+
+
+def time_average(function, coordinates, pool=None, verbose=False):
+    """
+    Compute the time average of a function.
+
+    Args:
+        function:
+            The function that will be averaged, it has to accept exactly one argument
+            which is the current atom set.
+        coordinates: The coordinates object of the simulation
+        pool (multiprocessing.Pool, opt.):
+            A multiprocessing pool which will be used for cocurrent calculation of the
+            averaged function.
+        verbose (bool, opt.): If
+
+    """
     coordinate_iter = iter(coordinates)
     number_of_averages = 0
     result = 0
@@ -19,26 +78,27 @@ def time_average(function, coordinates, pool=None):
     for ev in evaluated:
         number_of_averages += 1
         result += ev
-        if number_of_averages % 100 == 0:
+        if verbose and number_of_averages % 100 == 0:
             print(number_of_averages)
 
-    return result/number_of_averages
-    
+    return result / number_of_averages
+
+
 def time_histogram(function, coordinates, bins, hist_range, pool=None):
     coordinate_iter = iter(coordinates)
-    
+
     if pool is not None:
         _map = pool.imap
     else:
         _map = map
 
     evaluated = _map(function, coordinate_iter)
-    
+
     results = []
     hist_results = []
     for num, ev in enumerate(evaluated):
         results.append(ev)
-        
+
         if num % 100 == 0 and num > 0:
             print(num)
             r = np.array(results).T
@@ -61,45 +121,73 @@ def radial_pair_distribution(atoms, bins, box=None):
     diff = pbc_diff(atoms[indices[0]], atoms[indices[1]], box)
 
     dist = (diff**2).sum(axis=1)**.5
-    volume = 4/3*np.pi*(bins[1:]**3-bins[:-1]**3)
+    volume = 4 / 3 * np.pi * (bins[1:]**3 - bins[:-1]**3)
     hist = np.histogram(dist, bins)[0]
-    density = (number_of_atoms-1) / np.prod(box)
+    density = (number_of_atoms - 1) / np.prod(box)
 
-    return hist / volume / density * (2/(number_of_atoms-1))
+    return hist / volume / density * (2 / (number_of_atoms - 1))
 
 
 def distance_distribution(atoms, bins):
-    connection_vectors = atoms[:-1,:]-atoms[1:,:]
+    connection_vectors = atoms[:-1, :] - atoms[1:, :]
     connection_lengths = (connection_vectors**2).sum(axis=1)**.5
     return np.histogram(connection_lengths, bins)[0]
 
+
 def tetrahedral_order(atoms):
-    indices = next_neighbors(atoms, number_of_neighbors = 4)
+    indices = next_neighbors(atoms, number_of_neighbors=4)
     neighbors = atoms[indices]
     neighbors_1, neighbors_2, neighbors_3, neighbors_4 = \
-        neighbors[:,0,:], neighbors[:,1,:], neighbors[:,2,:], neighbors[:,3,:]
-    
+        neighbors[:, 0, :], neighbors[:, 1, :], neighbors[:, 2, :], neighbors[:, 3, :]
+
     # Connection vectors
     neighbors_1 -= atoms
     neighbors_2 -= atoms
     neighbors_3 -= atoms
     neighbors_4 -= atoms
-    
+
     # Normed Connection vectors
-    neighbors_1 /= ((neighbors_1**2).sum(axis=1)**.5).reshape(neighbors_1.shape[0],1)
-    neighbors_2 /= ((neighbors_2**2).sum(axis=1)**.5).reshape(neighbors_1.shape[0],1)
-    neighbors_3 /= ((neighbors_3**2).sum(axis=1)**.5).reshape(neighbors_1.shape[0],1)
-    neighbors_4 /= ((neighbors_4**2).sum(axis=1)**.5).reshape(neighbors_1.shape[0],1)
-    
-    a_1_2 = ((neighbors_1*neighbors_2).sum(axis=1) + 1/3)**2
-    a_1_3 = ((neighbors_1*neighbors_3).sum(axis=1) + 1/3)**2
-    a_1_4 = ((neighbors_1*neighbors_4).sum(axis=1) + 1/3)**2
-    
-    a_2_3 = ((neighbors_2*neighbors_3).sum(axis=1) + 1/3)**2
-    a_2_4 = ((neighbors_2*neighbors_4).sum(axis=1) + 1/3)**2
-     
-    a_3_4 = ((neighbors_3*neighbors_4).sum(axis=1) + 1/3)**2
-    
-    q = 1 - 3/8 * (a_1_2 + a_1_3 + a_1_4 + a_2_3 + a_2_4 + a_3_4)
-    
+    neighbors_1 /= ((neighbors_1**2).sum(axis=1)**.5).reshape(neighbors_1.shape[0], 1)
+    neighbors_2 /= ((neighbors_2**2).sum(axis=1)**.5).reshape(neighbors_1.shape[0], 1)
+    neighbors_3 /= ((neighbors_3**2).sum(axis=1)**.5).reshape(neighbors_1.shape[0], 1)
+    neighbors_4 /= ((neighbors_4**2).sum(axis=1)**.5).reshape(neighbors_1.shape[0], 1)
+
+    a_1_2 = ((neighbors_1 * neighbors_2).sum(axis=1) + 1 / 3)**2
+    a_1_3 = ((neighbors_1 * neighbors_3).sum(axis=1) + 1 / 3)**2
+    a_1_4 = ((neighbors_1 * neighbors_4).sum(axis=1) + 1 / 3)**2
+
+    a_2_3 = ((neighbors_2 * neighbors_3).sum(axis=1) + 1 / 3)**2
+    a_2_4 = ((neighbors_2 * neighbors_4).sum(axis=1) + 1 / 3)**2
+
+    a_3_4 = ((neighbors_3 * neighbors_4).sum(axis=1) + 1 / 3)**2
+
+    q = 1 - 3 / 8 * (a_1_2 + a_1_3 + a_1_4 + a_2_3 + a_2_4 + a_3_4)
+
     return q
+
+
+def radial_density(atoms, bins, symmetry_axis=(0, 0, 1), origin=(0, 0, 0), height=1):
+    """
+    Calculate the radial density distribution.
+
+    This function is meant to be used with time_average.
+
+    Args:
+        atoms:
+            Set of coordinates.
+        bins:
+            Bin specification that is passed to numpy.histogram. This needs to be
+            a list of bin edges if the function is used within time_average.
+        symmetry_axis (opt.):
+            Vector of the symmetry axis, around which the radial density is calculated,
+            default is z-axis.
+        origin (opt.):
+            Origin of the rotational symmetry, e.g. center of the pore.
+        height (opt.):
+            Height of the pore, necessary for correct normalization of the density.
+    """
+    cartesian = rotate_axis(atoms - origin, symmetry_axis)
+    radius, _ = polar_coordinates(cartesian[:, 0], cartesian[:, 1])
+    hist = np.histogram(radius, bins=bins)[0]
+    volume = 2 * np.pi * (bins[1:]**2 - bins[:-1]**2) * height
+    return hist / volume
