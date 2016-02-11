f7ac15769997089f5cb8466d5290b47c87fc4571
Niels Müller <post@nielsm.de>
2016-02-11 15:58:30 +0100

    New function to load a xtc file and generate an index file if necessary.

--- mdevaluate/__init__.py
+++ mdevaluate/__init__.py
@@ -1,1 +1,14 @@
-from . import atoms, coordinates, correlation, coordinates, distribution, evaluation, functions, gromacs, pbc, simulation 
+from . import atoms, coordinates, correlation, coordinates, distribution, evaluation, functions, gromacs, pbc, simulation
+import os
+
+def trajectory_from_xtc(xtc_file, generate_index=True):
+    """
+    Load a trajectory from a xtc file. If no index file is found,
+    a new one will be generated if generate_index is true.
+    """
+    index_file = gromacs.reader.index_filename_for_xtc(xtc_file)
+    if not os.path.exists(index_file):
+        print('No index file found, generating a new one. This may take a while...')
+        gromacs.xtcindex.index_xtcfile(xtc_file)
+
+    return gromacs.reader.XTCReader(xtc_file)
