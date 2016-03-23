70da88dfa4f3b1d619709016da0702db44009c9f
Niels Müller <post@nielsm.de>
2016-03-23 16:06:33 +0100

    Print out name of the topology file that is used.

--- mdevaluate/__init__.py
+++ mdevaluate/__init__.py
@@ -1,7 +1,7 @@
 import os
 from glob import glob
 
-from . import atoms, coordinates, correlation, coordinates, distribution, evaluation, functions, gromacs, pbc, simulation, autosave
+from . import atoms, coordinates, correlation, coordinates, distribution, functions, gromacs, pbc, simulation, autosave
 
 
 def trajectory_from_xtc(xtc_file, generate_index=True):
@@ -22,14 +22,17 @@ def load_simulation(directory, xtc='*.xtc', tpr='*.tpr', gro='*.gro'):
     Load a simulation from adirectory.
     """
     tpr_glob = glob(os.path.join(directory, tpr))
+    gro_glob = glob(os.path.join(directory, gro))
     if tpr_glob is not None and len(tpr_glob) is 1:
-        print('Loading tpology: {}'.format(tpr_glob[0]))
+        print('Loading topology: {}'.format(tpr_glob[0]))
         atom_set = atoms.from_tprfile(tpr_glob[0])
+    elif gro_glob is not None and len(gro_glob) is 1:
+        print('Loading topology: {}'.format(gro_glob[0]))
+        atom_set = atoms.from_grofile(gro_glob[0])
     else:
-        gro_file, = glob(os.path.join(directory, gro))
-        atom_set = atoms.from_grofile(gro_file)
+        raise FileNotFoundError('Topology file could not be identified.')
     xtc_file, = glob(os.path.join(directory, xtc))
     print('Loading trajectory: {}'.format(xtc_file))
     frames = trajectory_from_xtc(xtc_file)
-    
+
     return coordinates.Coordinates(frames, atom_subset=atom_set)
