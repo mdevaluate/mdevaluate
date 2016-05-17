808f8de981897b12dde1a0d8af41f8be1ce395c1
Niels Müller <niels.mueller@physik.tu-darmstadt.de>
2016-05-17 17:02:27 +0200

    Verbose name for hav-neg

--- mdevaluate/functions.py
+++ mdevaluate/functions.py
@@ -18,6 +18,6 @@ def cole_cole(w, A, b, t0):
     return A*(w*t0)**b * np.sin(np.pi*b/2) / (1 + 2*(w*t0)**b * np.cos(np.pi*b/2) + (w*t0)**(2*b))
 
 
-def hav_neg(w, A, e, d, t0):
+def havriliak_negami(w, A, e, d, t0):
     P = np.arctan((w*t0)**d * np.sin(np.pi*d/2) / (1 + (w*t0)**d * np.cos(np.pi*d/2)))
     return A * np.sin(e * P) / (1 + 2*(w*t0)**d * np.cos(np.pi*d/2) + (w*t0)**(2*d))**(e/2)
