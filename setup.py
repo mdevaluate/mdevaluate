4e9b84ad27707d3791a146e9466fe0129a4ddcfd
Niels Müller <niels.mueller@physik.tu-darmstadt.de>
2016-12-01 16:04:41 +0100

    Do not import module in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup
-from mdevaluate import __version__
+
+
+def get_version(module):
+    version = ''
+    with open(module) as f:
+        for line in f:
+            if '__version__' in line:
+                version = line.split('=')[-1].strip("' \n\t")
+                break
+    return version
+
+__version__ = get_version('mdevaluate/__init__.py')
 
 setup(
     name='mdevaluate',
