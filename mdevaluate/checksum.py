fc52d320e8d433b2292e3ee6a9a7b2403980be56
Niels Müller <niels.mueller@physik.tu-darmstadt.de>
2018-07-12 09:43:29 +0200

    Use name of module only to determine the checksum (as oposed to str(module) that includes the file path).

--- mdevaluate/checksum.py
+++ mdevaluate/checksum.py
@@ -2,6 +2,7 @@
 import functools
 import hashlib
 from .logging import logger
+from types import ModuleType
 
 import numpy as np
 
@@ -12,7 +13,6 @@ SALT = 42
 
 def version(version_nr, calls=[]):
     """Function decorator that assigns a custom checksum to a function."""
-
     def decorator(func):
         cs = checksum(func.__name__, version_nr, *calls)
         func.__checksum__ = lambda: cs
@@ -27,19 +27,30 @@ def version(version_nr, calls=[]):
 
 def checksum(*args):
     """
-    Calculate the checksum of any object.
+    Calculate a checksum of any object, by md5 hash.
+
+    Input for the hash are some salt bytes and the byte encoding of a string 
+    that depends on the object and its type:
+
+    - If a method __checksum__ is available, it's return value is converted to bytes
+    - str or bytes are used as md5 input directly
+    - modules use the __name__ attribute
+    - functions use the function code and any closures of the function
+    - functools.partial uses the checksum of the function and any arguments, that were defined
+    - numpy.ndarray uses bytes representation of the array (arr.tobytes())
+    - Anything else is converted to a str
     """
     bstr = str(SALT).encode()
     for arg in args:
         if hasattr(arg, '__checksum__'):
             logger.debug('Checksum via __checksum__: %s', str(arg))
             bstr += str(arg.__checksum__()).encode()
-        elif arg is None:
-            bstr += b'None'
         elif isinstance(arg, bytes):
             bstr += arg
         elif isinstance(arg, str):
             bstr += arg.encode()
+        elif isinstance(arg, ModuleType):
+            bstr += arg.__name__.encode()
         elif hasattr(arg, '__code__'):
             logger.debug('Checksum via __code__ for %s', str(arg))
             bstr += arg.__code__.co_code
