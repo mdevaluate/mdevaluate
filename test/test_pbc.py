970aa3726999ac72c072a611acb61320d66abc11
Niels Müller <niels.mueller@physik.tu-darmstadt.de>
2018-07-12 09:45:57 +0200

    Fix tests

--- test/test_pbc.py
+++ test/test_pbc.py
@@ -9,6 +9,6 @@ def test_pbc_diff():
     y = np.random.rand(10, 3)
     box = np.ones((3,))
 
-    assert (pbc.pbc_diff(x, x, box) == approx(0)).all()
+    assert (pbc.pbc_diff(x, x, box) == approx(0))
     dxy = (pbc.pbc_diff(x, y, box)**2).sum(axis=1)**0.5
     assert (dxy <= 0.75**0.5).all()
