bae75db882cf436889b818aa317d3022cda959df
simonhef <42342724+simonhef@users.noreply.github.com>
2019-02-19 11:32:31 +0100

    Added an Hbond class
    Added an H-bond class for easier access and a list class to avoid crashes.

--- mdevaluate/Hbonds.py
+++ mdevaluate/Hbonds.py
@@ -3,7 +3,20 @@ import scipy
 from mdevaluate.pbc import pbc_diff
 import sys
 import collections
-import multiprocessing
+from functools import lru_cache
+
+def recursive_len(item):
+        if type(item)==str:
+            return len(item)
+        if ((type(item) == list) or (type(item) == tuple) or (type(item)==dict)):
+            return sum(recursive_len(subitem) for subitem in item)
+        else:
+            return 1
+class MyList(list):
+    def __init__(self, *args):
+        list.__init__(self, *args)
+    def __repr__(self):
+        return "Number of charakters = "+str(recursive_len(list(self)))
 
 def Hbondlist(don,H_,akz,maxangle=30,maxdistance=0.35):
     """
@@ -41,6 +54,8 @@ def Veranderungsliste(donliste,Hliste,akzliste,donqty=1):
         donliste: The subset of a Trajectory with the donor-atom of each donor-group.
         Hliste: The subset of a Trajectory with the hydrogen atom of each donor-group.
         akzliste: The subset of a Trajectory with all acceptor atoms.
+        donqty: The number of hydrogen each donor atom has.
+        Returns during the calculation the current frame in steps of 100.
     """
     mask=[]
     for i in range(len(donliste[0])):
@@ -52,7 +67,7 @@ def Veranderungsliste(donliste,Hliste,akzliste,donqty=1):
     start = Hbondlist(donliste[0][mask],Hliste[0],akzliste[0])
     Veranderung = [""]*len(donliste[0][mask])
     vergleich1 = start
-    while fn < len(donliste) :
+    while fn < 1000 :
         if fn%100 == 0:
             print(fn,end = '\r')
         vergleich2 = Hbondlist(donliste[fn][mask],Hliste[fn],akzliste[fn])
@@ -65,13 +80,13 @@ def Veranderungsliste(donliste,Hliste,akzliste,donqty=1):
     
         vergleich1 = vergleich2
         fn += 1
-    return Veranderung , start
+    return MyList(Veranderung) , start
 
 
-def spatiallist(donliste,Hliste,akzliste,bins,radius,donqty=1):
+def spatiallist(donliste,Hliste,akzliste,bins,origin,radius,donqty=1,maxheight=np.Inf,):
     """
     Returns an array with shape (bins,frames) which contains the average number of Hydrogen bonds in the area around
-    the z-axiswith equally distanced sections. The average number is then stored in the array with respect to the distance to the z-axis in the 
+    the z-axis with equally distanced sections. The average number is then stored in the array with respect to the distance to the z-axis in the 
     middle of the box.
     Args:
         donliste: The subset of a Trajectory with the donor-atom of each donor-group.
@@ -93,17 +108,17 @@ def spatiallist(donliste,Hliste,akzliste,bins,radius,donqty=1):
     while fn < frames:
         if fn%100 == 0:
             print(fn,end = '\r')
-        point = np.array([0.5*Hliste[fn].box[0][0],0.5*Hliste[fn].box[1][1],0])
+        point = origin
         diff = Hliste[fn]-point
         distance=np.linalg.norm(diff-((diff)*unit)*unit,axis=1)
         areas = np.array(range(bins+1))/bins*radius
         masks =[]
         i = 0
         while i < bins:
-            masks.append(((distance>=areas[i]) & (distance<areas[i+1])))
+            masks.append(((distance>=areas[i]) & (distance<areas[i+1]) & (Hliste[fn][:,2]<maxheight)))
             i+=1
         liste = Hbondlist(donliste[fn][donorquantity],Hliste[fn],akzliste[fn])
-        m = 0
+        m = 0                                                                                                                                                                                                       
         while m < bins:
             for k in liste[masks[m]]:
                 spatialarr[m][fn]+=len(k)
@@ -112,18 +127,18 @@ def spatiallist(donliste,Hliste,akzliste,bins,radius,donqty=1):
         fn+=1
     return spatialarr
 
-def getaverageHbond(startlist,changelist,framenumber):
+def getaverageHbond(startlist,changelist,numberofframes):
     """
     Return the an array with shape(donornr,framenumber) which contains how many hydrogen bonds each donor in each frame has.
     Args:
         startlist: The list of Hydrogenbonds of each donor-group for the first frame.
         changelist: The changes of hydrogen-bonds for each frame for each donor-group.
-        framenumber: The number of frames in the trajectory.
+        numberofframes: The number of frames in the trajectory.
     """
-    lifetime = np.zeros(len(startlist),framenumber,dtype=np.int8)
+    lifetime = np.zeros((len(startlist),numberofframes),dtype=np.int8)
     start = startlist
     k = 0
-    while k<len(framenumber):
+    while k<len(startlist):
         sys.stdout.flush()
         if k%100 == 0:
             print(k,end='\r')
@@ -133,6 +148,7 @@ def getaverageHbond(startlist,changelist,framenumber):
         i = 0
         for t in temp:
             temp2 = [l.split(',') for l in t[0].split(':')]
+            temp2 = [l.split(',') for l in t[0].split(':')]
             while i< int(temp2[0][0]):
                 lifetime[k][i] = len(Bindungen)
                 i+=1
@@ -140,7 +156,7 @@ def getaverageHbond(startlist,changelist,framenumber):
                 Bindungen.remove(int(temp2[1][0]))
             else:
                 Bindungen.add(int(temp2[1][0]))
-        while i<framenumber:
+        while i<numberofframes:
             lifetime[k][i] = len(Bindungen)
             i+=1
         k +=1
@@ -197,7 +213,7 @@ def HlifePlateus(startlist,changelist,frames,average=3):
     mean = average
     final = collections.Counter()
     j=0
-    while j<len(Veranderung):  
+    while j<frames:  
         if j%100==0:
             print(j,end='\r')
         sortlist, akzlist = getlists(start,Veranderung,j)
@@ -234,3 +250,44 @@ def HlifePlateus(startlist,changelist,frames,average=3):
             final.update(m[1::2]-m[::2])
         j+=1
     return final
+
+class Hbond():
+    
+    def __init__(self, donlist, Hlist, akzlist,donqty=1):
+        self.akz=akzlist
+        self.don=donlist
+        self.H=Hlist
+        self.donqty=donqty
+        self.frames=len(Hlist)
+        self.change,self.start=Veranderungsliste(Hliste=Hlist,akzliste=akzlist,donliste=donlist,donqty=donqty)
+    @lru_cache(maxsize=10)
+    def averageHbond(self):
+        """
+        Returns the average number of hydrogen bonds per donor group over the whole trajectory for each frame.
+        """
+        return np.mean(getaverageHbond(changelist=self.change,startlist=self.start,numberofframes=self.frames),axis=1)
+    @lru_cache(maxsize=10)
+    def averageHbondmol(self):
+        """
+        Returns the average number of hydrogen bonds for each donor group over the whole trajectory for each frame.
+        """
+        return getaverageHbond(changelist=self.change,startlist=self.start,numberofframes=self.frames)
+    @lru_cache(maxsize=10)
+    def lifetimedist(self,average=3):
+        """
+        Returns a Counter object with the number of lifetimes of the hydrogen bonds in the trajectory.
+        Args:
+            average: The minimum frame difference between a hydrogen bonding to count it as seperate.
+        """
+        return HlifePlateus(startlist=self.start,changelist=self.change,average=average,frames=self.frames)
+    @lru_cache(maxsize=10)
+    def Hbondlistmol(self,donnumber):
+        """
+        Returns two lists of a specific donor group. The first list returns all changes of the donor ordered by acceptor numbers,
+        the second list are the numbers of all acceptors which have been interacted with.
+        Args:
+            donnumber: The number of the donor group which lists are calculated.
+        """
+        return getlists(changelist=self.change,startlist=self.start,donnumber=donnumber)
+    
+    
