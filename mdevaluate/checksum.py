5fab4284b7edd08c760165257da1af1e14609256
Niels Mueller <niels.mueller@physik.tu-darmstadt.de>
2019-10-08 15:18:15 +0200

    Better checksum calucaltion

--- mdevaluate/checksum.py
+++ mdevaluate/checksum.py
@@ -2,7 +2,8 @@
 import functools
 import hashlib
 from .logging import logger
-from types import ModuleType
+from types import ModuleType, FunctionType
+import inspect
 
 import numpy as np
 
@@ -25,7 +26,19 @@ def version(version_nr, calls=[]):
     return decorator
 
 
-def checksum(*args):
+def strip_comments(s):
+    """Strips comment lines and docstring from Python source string."""
+    o = ''
+    in_docstring = False
+    for l in s.split('\n'):
+        if l.strip().startswith(('#', '"', "'")) or in_docstring:
+            in_docstring = l.strip().startswith(('"""', "'''")) + in_docstring == 1
+            continue
+        o += l + '\n'
+    return o
+
+
+def checksum(*args, csum=None):
     """
     Calculate a checksum of any object, by md5 hash.
 
@@ -40,36 +53,45 @@ def checksum(*args):
     - numpy.ndarray uses bytes representation of the array (arr.tobytes())
     - Anything else is converted to a str
     """
-    bstr = str(SALT).encode()
+    if csum is None:
+        csum = hashlib.sha1()
+        csum.update(str(SALT).encode())
+
     for arg in args:
         if hasattr(arg, '__checksum__'):
             logger.debug('Checksum via __checksum__: %s', str(arg))
-            bstr += str(arg.__checksum__()).encode()
+            csum.update(str(arg.__checksum__()).encode())
         elif isinstance(arg, bytes):
-            bstr += arg
+            csum.update(arg)
         elif isinstance(arg, str):
-            bstr += arg.encode()
+            csum.update(arg.encode())
         elif isinstance(arg, ModuleType):
-            bstr += arg.__name__.encode()
-        elif hasattr(arg, '__code__'):
-            logger.debug('Checksum via __code__ for %s', str(arg))
-            bstr += arg.__code__.co_code
-            if arg.__closure__ is not None:
-                for cell in arg.__closure__:
-                    bstr += str(checksum(cell.cell_contents)).encode()
+            csum.update(arg.__name__.encode())
+        elif isinstance(arg, FunctionType):
+            csum.update(strip_comments(inspect.getsource(arg)).encode())
+            c = inspect.getclosurevars(arg)
+            for v in {**c.nonlocals, **c.globals}.values():
+                if v is not arg:
+                    checksum(v, csum=csum)
+#        elif hasattr(arg, '__code__'):
+#            logger.debug('Checksum via __code__ for %s', str(arg))
+#            bstr += arg.__code__.co_code
+#            if arg.__closure__ is not None:
+#                for cell in arg.__closure__:
+#                    bstr += str(checksum(cell.cell_contents)).encode()
         elif isinstance(arg, functools.partial):
             logger.debug('Checksum via partial for %s', str(arg))
-            bstr += str(checksum(arg.func)).encode()
+            checksum(arg.func, csum=csum)
             for x in arg.args:
-                bstr += str(checksum(x)).encode()
+                checksum(x, csum=csum)
             for k in sorted(arg.keywords.keys()):
-                bstr += k.encode() + str(checksum(arg.keywords[k])).encode()
+                csum.update(k.encode())
+                checksum(arg.keywords[k], csum=csum)
         elif isinstance(arg, np.ndarray):
-            bstr += arg.tobytes()
+            csum.update(arg.tobytes())
         else:
             logger.debug('Checksum via str for %s', str(arg))
-            bstr += str(arg).encode()
+            csum.update(str(arg).encode())
 
-    m = hashlib.md5()
-    m.update(bstr)
-    return int.from_bytes(m.digest(), 'big')
+    return int.from_bytes(csum.digest(), 'big')
+    
